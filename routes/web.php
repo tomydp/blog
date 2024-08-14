180b0ab398956a42a3ac8f13d0359ced51b8e63e
tomyvdr <145481952+tomyvdr@users.noreply.github.com>
2024-08-13 21:11:28 -0300

    Rutas

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,18 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::view('/', 'welcome')->name('home');
+Route::view('/contacto', 'contact')->name('contact');
+Route::view('/blog', 'blog')->name('blog');
+Route::view('/nosotros', 'about')->name('about');
+
+// Route::match(['put', 'patch'], '/', function() {
+    // Usar varias Peticiones y "ANY" para todas
+// });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
